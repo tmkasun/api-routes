5141576379bdcf6cc970739a62799fc478704a91
Kasun Thennakoon <tmkasun@gmail.com>
2021-11-20 15:56:59 +0000

    new try

---
+++ api/index.js
@@ -0,0 +1,43 @@
+const app = require('express')();
+import { OpenAPIBackend } from 'openapi-backend';
+
+
+const api = new OpenAPIBackend({
+    definition: "https://raw.githubusercontent.com/wso2/carbon-apimgt/tmkasun-patch-1/components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1/src/main/resources/publisher-api.yaml",
+  });
+  
+  api.register('notFound', (c, res, ctx) => res(ctx.status(404)));
+  api.register('notImplemented', async (c, res, ctx) => {
+    const { status, mock } = await api.mockResponseForOperation(c.operation.operationId);
+    return res(ctx.status(status), ctx.json(mock));
+  });
+  api.register('validationFail', (c, req, res) => res.status(400).json({ err: c.validation.errors }))
+
+app.get('/api', async (req, res) => {
+  const path = `/api/item/${v4()}`;
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
+  const oasRequest = {
+    path: '/apis',
+    method: 'GET'
+  };
+  mockedResponse = await api.handleRequest(oasRequest, req, res);
+  console.log(mockedResponse)
+  return mockedResponse;
+});
+
+module.exports = app;
+
+
+
+// export default async function handler(req, res) {
+//   const { method } = req
+//   const path = `/${req.query.index.join('/')}`
+//   const oasRequest = {
+//     path: '/apis',
+//     method: 'GET'
+//   };
+//   mockedResponse = await api.handleRequest(oasRequest, req, res);
+//   debugger
+//   res.status(200).json({ "a": 'asd' })
+// }
