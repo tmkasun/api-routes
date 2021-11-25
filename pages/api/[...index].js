73fad0fb8b1c4b073f4536323d8a586f61845c3d
Kasun Thennakoon <tmkasun@gmail.com>
2021-11-25 03:01:25 +0000

    simple heello

--- pages/api/[...index].js
+++ pages/api/[...index].js
@@ -1,23 +1,3 @@
-import { OpenAPIBackend } from 'openapi-backend';
-
-const api = new OpenAPIBackend({
-  definition: "https://raw.githubusercontent.com/wso2/carbon-apimgt/tmkasun-patch-1/components/apimgt/org.wso2.carbon.apimgt.rest.api.publisher.v1/src/main/resources/publisher-api.yaml",
-});
-
-// api.register('notFound', (c, res, ctx) => res(ctx.status(404)));
-api.register('notImplemented', async (c, res, ctx) => {
-  const { status, mock } = await api.mockResponseForOperation(c.operation.operationId);
-  return res(ctx.status(status), ctx.json(mock));
-});
-api.register('validationFail', (c, req, res) => res.status(400).json({ err: c.validation.errors }))
-export default async function handler(req, res) {
-  const { method } = req
-  const path = `/${req.query.index.join('/')}`
-  const oasRequest = {
-    path: '/apis',
-    method: 'GET'
-  };
-  mockedResponse = await api.handleRequest(oasRequest, req, res);
-  debugger
-  res.status(200).json({ "a": 'asd' })
-}
+module.exports = function (req, res) {
+  res.json({"data": `Hello world! Time = ${Date.now()}`});
+};
